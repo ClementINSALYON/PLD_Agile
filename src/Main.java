e3555a6b803853dff01dadbc2e7d12edd8850256
ClementINSALYON <clement.bousseau@insa-lyon.fr>
2022-10-20 23:16:37 +0200

    commiting on dev branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world on branch devvvv!");
+        System.out.println("Hello world on branch dev2!");
     }
 }
